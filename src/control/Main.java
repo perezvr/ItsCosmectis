7e938f9428312899d4a27863e2bd1fcca489d156
perezvr <perez.vr@gmail.com>
2015-04-23 21:47:35 -0300

    teste dr gravacao de endereco

--- src/control/Main.java
+++ src/control/Main.java
@@ -7,8 +7,10 @@ package control;
 
 import error.ValidarException;
 import java.util.Date;
+import model.Cidades;
 import model.Clientes;
 import model.ClientesFisicos;
+import model.Enderecos;
 
 /**
  *
@@ -17,13 +19,29 @@ import model.ClientesFisicos;
 public class Main {
 
     public static void main(String[] args) throws ValidarException {
+//        Clientes cliente = new Clientes();
+//        ClientesFisicos cliFis = new ClientesFisicos();
+//        cliente.setCliTipoCliente('F');
+//        cliFis.setCliNome("Maurílio2");
+//        cliFis.setCliSexo('M');
+//        cliFis.setCliDataNascimento(new Date("01/01/2014"));
+//        cliFis.setClientes(cliente);
+//        ControleCliente.gravaClienteFisico(cliFis);
+
+        Enderecos end = new Enderecos();
+        Cidades cidade = new Cidades();
+        cidade.setIdCidade(1);
         Clientes cliente = new Clientes();
-        ClientesFisicos cliFis = new ClientesFisicos();
-        cliente.setCliTipoCliente('F');
-        cliFis.setCliNome("Maurílio2");
-        cliFis.setCliSexo('M');
-        cliFis.setCliDataNascimento(new Date("01/01/2014"));
-        cliFis.setClientes(cliente);
-        ControleCliente.gravaClienteFisico(cliFis);
+        cliente.setIdCliente(1);
+
+        end.setEndLogradouro("Rua Cesar Lattes");
+        end.setEndNumero("57");
+        end.setEndComplemento("ap 403");
+        end.setEndBairro("Limeiro");
+        end.setEndCep("27281410");
+        end.setCidadesIdCidade(cidade);
+        end.setClientesIdCliente(cliente);
+        ControleEndereco.gravaEndereco(end);
+
     }
 }
